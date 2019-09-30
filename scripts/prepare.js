e60e92c77a8859c6c63eb5f81a089b8971e4ea03
Björn Aili <bjorn.aili@gmail.com>
2019-09-29 21:31:39 -0700

    Add step to delete empty declarations in package script.

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -3,9 +3,28 @@ const path = require("path")
 
 const packageContent = ["package.json", "README.md", "LICENSE"]
 
+console.log("Deleting empty declarations")
+deleteEmpties("dist")
+
 console.log("Preparing package contents")
 
 packageContent.forEach(file => {
     console.log(path.resolve(file))
     fs.copyFileSync(file, path.join("dist", file))
 })
+
+function deleteEmpties(folder) {
+    const files = fs.readdirSync(folder)
+    files.forEach(entry => {
+        entry = path.join(folder, entry)
+        if (entry.match(/\.d\.ts$/)) {
+            const contents = fs.readFileSync(entry)
+            if (contents.toString().match(/^export {};[\r\n]+$/)) {
+                console.log(`- ${entry}`)
+                fs.unlinkSync(entry)
+            }
+        } else if (fs.lstatSync(entry).isDirectory()) {
+            deleteEmpties(entry)
+        }
+    })
+}
